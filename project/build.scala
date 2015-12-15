74adddb1a7033eeb1a12fd17ac0017bb55e14ca4
Naoki Takezoe <takezoe@gmail.com>
2015-12-15 19:40:08 +0900

    Fix for Bootstrap3

--- project/build.scala
+++ project/build.scala
@@ -7,7 +7,7 @@ object MyBuild extends Build {
 
   val Organization = "jp.sf.amateras"
   val Name = "gitbucket-gist-plugin"
-  val Version = "3.7.0"
+  val Version = "3.10.0"
   val ScalaVersion = "2.11.6"
 
   lazy val project = Project (
@@ -25,7 +25,7 @@ object MyBuild extends Build {
       "amateras-repo" at "http://amateras.sourceforge.jp/mvn/"
     ),
     libraryDependencies ++= Seq(
-      "gitbucket"          % "gitbucket-assembly" % "3.7.0" % "provided",
+      "gitbucket"          % "gitbucket-assembly" % "3.9.0" % "provided",
       "com.typesafe.play" %% "twirl-compiler"     % "1.0.4" % "provided",
       "javax.servlet"      % "javax.servlet-api"  % "3.1.0" % "provided"
     ),
