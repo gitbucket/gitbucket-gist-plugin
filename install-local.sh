30e3ccbd941a9f76e5d6545cc35e5ae854528a93
Naoki Takezoe <takezoe@gmail.com>
2016-07-09 21:44:12 +0900

    Bump to 4.2.0

--- install-local.sh
+++ install-local.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ./sbt.sh package
-cp target/scala-2.11/gitbucket-gist-plugin_2.11-4.0.0.jar ~/.gitbucket/plugins/
+cp target/scala-2.11/gitbucket-gist-plugin_2.11-4.2.0.jar ~/.gitbucket/plugins/
