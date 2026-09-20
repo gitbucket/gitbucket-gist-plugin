c8caa3d5801451cbe90cd6e53d22bdfd58ee955e
takezoe <takezoe@gmail.com>
2026-09-20 14:55:14 +0900

    Release 4.26.0

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -47,7 +47,8 @@ class Plugin extends gitbucket.core.plugin.Plugin {
     new Version("4.22.0"),
     new Version("4.23.0"),
     new Version("4.24.0"),
-    new Version("4.25.0")
+    new Version("4.25.0"),
+    new Version("4.26.0")
   )
 
   override def initialize(registry: PluginRegistry, context: ServletContext, settings: SystemSettings): Unit = {
